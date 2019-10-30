b66fc052dd2adc1646416c02d65af504c006306b
Yongjoo Park <pyongjoo@umich.edu>
2019-10-30 05:26:13 +0000

    updated notes

--- docker/pull_notes_from_docker.sh
+++ docker/pull_notes_from_docker.sh
@@ -2,6 +2,7 @@
 
 docker cp docker-verdict:/opt/notebooks/NoVerdict.ipynb jupyter_notebook/
 docker cp docker-verdict:/opt/notebooks/WithVerdict.ipynb jupyter_notebook/
+docker cp docker-verdict:/opt/notebooks/Instacart.ipynb jupyter_notebook/
 mkdir -p zeppelin_notebook/2EMM5HJDB zeppelin_notebook/2ESF1KHY9
 docker cp docker-verdict:/root/zeppelin-0.8.1-bin-all/notebook/2EMM5HJDB/note.json zeppelin_notebook/2EMM5HJDB/note.json
 docker cp docker-verdict:/root/zeppelin-0.8.1-bin-all/notebook/2ESF1KHY9/note.json zeppelin_notebook/2ESF1KHY9/note.json
